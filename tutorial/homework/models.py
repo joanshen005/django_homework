10730bc225380324947af178fe9249baac3479e8
Joan <joanshen005@gmail.com>
2025-07-14 11:38:46 +0800

    feat: homework_restfulAPI

---
+++ tutorial/homework/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=32)
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=32)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
---
+++ tutorial/homework/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from .models import Student, Teacher
+
+class StudentSerializer(serializers.ModelSerializer):
+    teacher_name = serializers.CharField(source='teacher.name', read_only=True)
+
+    class Meta:
+        model = Student
+        fields = ('id', 'name', 'teacher', 'teacher_name')
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'

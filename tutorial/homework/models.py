2d055384219858f64ef9b3e4a5c27f0f9fb33dc7
Joan <joanshen005@gmail.com>
2025-07-15 20:25:54 +0800

    feat: add subject column in class Teacher

--- tutorial/homework/models.py
+++ tutorial/homework/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Teacher(models.Model):
     name = models.CharField(max_length=32)
+    subject = models.CharField(max_length=32, null=True)
     def __str__(self):
         return self.name
 
--- tutorial/homework/serializers.py
+++ tutorial/homework/serializers.py
@@ -4,13 +4,15 @@ from .models import Student, Teacher
 
 class StudentSerializer(serializers.ModelSerializer):
     teacher_name = serializers.CharField(source='teacher.name', read_only=True)
+    teacher_subject = serializers.CharField(source='teacher.subject', read_only=True)
 
     class Meta:
         model = Student
-        fields = ('id', 'name', 'teacher', 'teacher_name')
+        fields = ('id', 'name', 'teacher', 'teacher_name', 'teacher_subject')
 
 
 class TeacherSerializer(serializers.ModelSerializer):
+    # students = StudentSerializer(source='student_set', many=True, read_only=True)
     class Meta:
         model = Teacher
         fields = '__all__'
